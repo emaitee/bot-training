deb2fd8c519abc6c07d1a0f6bc3d041d6d389efd
emaitee <issatoyin@gmail.com>
2020-02-16 20:32:36 +0100

    add state machine

---
+++ ml_stateful.py
@@ -0,0 +1,42 @@
+# Define the states
+INIT=0
+AUTHED=1
+CHOOSE_COFFEE=2
+ORDERED=3
+
+# Define the policy rules
+policy_rules = {
+    (INIT, "order"): (INIT, "you'll have to log in first, what's your phone number?", AUTHED),
+    (INIT, "number"): (AUTHED, "perfect, welcome back!", None),
+    (AUTHED, "order"): (CHOOSE_COFFEE, "would you like Colombian or Kenyan?", None),    
+    (CHOOSE_COFFEE, "specify_coffee"): (ORDERED, "perfect, the beans are on their way!", None)
+}
+
+# Define send_messages()
+def send_messages(messages):
+    state = INIT
+    pending = None
+    for msg in messages:
+        state, pending = send_message(state, pending, msg)
+
+# Send the messages
+send_messages([
+    "I'd like to order some coffee",
+    "555-1234",
+    "kenyan"
+])
+
+##### Chit chat
+# Define chitchat_response()
+def chitchat_response(message):
+    # Call match_rule()
+    response, phrase = match_rule(eliza_rules, message)
+    # Return none if response is "default"
+    if response == "default":
+        return None
+    if '{0}' in response:
+        # Replace the pronouns of phrase
+        phrase = replace_pronouns(phrase)
+        # Calculate the response
+        response = response.format(phrase)
+    return response
